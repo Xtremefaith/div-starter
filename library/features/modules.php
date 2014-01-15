315994cd1b5134ba68974aec9c3e6ee590fa7826
Nick Worth <nick@divtruth.com>
2014-01-15 10:16:41 -0700

    reverse #4f9473, change enable/disable options

--- library/features/modules.php
+++ library/features/modules.php
@@ -1,56 +1,83 @@
 <?php 
 /*
-Feature Name:   Menu
-Developer URI:  http://www.DivTruth.com
-Description:    All custom WP menu/page options
-Version:        1.2
+Feature Name:   Custom Post Types
+Description:    All CPT activations are setup and installed here
+Dependencies:   ACF
 */
 
-/*****************************
- * Setup Custom Pages & Menus
- *****************************/
-if(function_exists("register_options_page")){
-    register_options_page('Theme Settings');
-    register_options_page('Company Settings');
-    register_options_page('Modules Settings');
-    register_options_page('Documentation');    
-    register_options_page('Developer Tools');    
-}
+/******************************************************
+ * DIRECTIONS: Activation Setup and Install Modules 
+ * 		by uncommenting the modules that you want turned
+ *		on for this particular theme
+ ******************************************************/
+global $theme_modules;
+if(file_exists(THEME_MODULES_DIR))
+    $theme_modules = div_dir_array(THEME_MODULES_DIR);
+$fields = array();
 
-/* ACTIONS */
-add_action('admin_menu', 'div_option_page_menus');  # Div Truth Options Menu
-add_action('admin_menu', 'hide_acf_admin_menu',999);  # Div Truth Options Menu
+if($theme_modules){
+    foreach ($theme_modules as $module => $path) {
+        $slug = CUSTOM::slugify($module);
+    	$cpt = CUSTOM::singularize_slug($module);
+    	$field = array(
+    		'key' => 'field_'.$slug,
+            'label' => 'Enable '.$module.' Module',
+            'name' => $slug.'_module',
+            'type' => 'radio',
+            'instructions' => 'Do you want to enable the '.$module.' module?',
+            'choices' => array (
+                    'Disable' => 'Disable',
+                    'Enable' => 'Enable',
+            ),
+            'other_choice' => 0,
+            'save_other_choice' => 0,
+            'default_value' => 'Disable',
+            'layout' => 'horizontal',
+        );
+        array_push($fields, $field);
 
-/* FUNCTIONS */
-function hide_acf_admin_menu() {
-    # If "Theme Develop Mode" is disabled
-    if(get_field('develop_mode','options') == "Disable"){
-      remove_menu_page('edit.php?post_type=acf'); # Remove ACF Menu
-      remove_submenu_page('admin.php?page=acf-options-theme-settings','admin.php?page=acf-options-developer-tools'); # Remove Developer Tools
+        if(file_exists(THEME_MODULES_DIR.'/'.$module.'/_'.$cpt.'.php'))
+            require_once( THEME_MODULES_DIR.'/'.$module.'/_'.$cpt.'.php');
+        
+        if(file_exists(THEME_MODULES_DIR.'/'.$module.'/fields.php'))
+            require_once( THEME_MODULES_DIR.'/'.$module.'/fields.php');
+
+        if(file_exists(THEME_MODULES_DIR.'/'.$module.'/columns.php'))
+            require_once( THEME_MODULES_DIR.'/'.$module.'/columns.php');
+
+        if(file_exists(THEME_MODULES_DIR.'/'.$module.'/permalinks.php'))
+            require_once( THEME_MODULES_DIR.'/'.$module.'/permalinks.php');
+
+        if(file_exists(THEME_MODULES_DIR.'/'.$module.'/shortcodes.php'))
+            require_once( THEME_MODULES_DIR.'/'.$module.'/shortcodes.php');
     }
-    # If "Documentation Page" is disabled
-    if(get_field('div_documentation_page','options') == "Disable"){
-      remove_submenu_page('admin.php?page=acf-options-theme-settings','documentation');
-    }
 }
 
-function div_option_page_menus() {
-  $menu_label = (get_field('div_menu_label','option')) ? get_field('div_menu_label','option') : 'Div Starter Options';
-  if(!file_exists(THEME_LIBRARY_URL.'/images/admin-logo.png')){
-    $menu_icon = THEME_LIBRARY_URL.'/images/admin-logo.png';
-  } else {
-    $menu_icon = DIV_LIBRARY_URL.'/images/admin-logo.png';
-  }
-  
-  add_menu_page("<span class='DT_title'>".$menu_label."</span>", "<span class='DT_title'>".$menu_label."</span>", 'manage_options','admin.php?page=acf-options-theme-settings',"",$menu_icon,4);
-  add_submenu_page('admin.php?page=acf-options-theme-settings', __('Developer Tools','theme-main'), __('Developer Tools','theme-main'), 'manage_options', 'admin.php?page=acf-options-developer-tools');
-  add_submenu_page('admin.php?page=acf-options-theme-settings', __('Documentation','theme-main'), __('Documentation','theme-main'), 'manage_options', 'documentation', 'div_active_pages');
-  add_submenu_page('admin.php?page=acf-options-theme-settings', __('Company Settings','theme-main'), __('Company Settings','theme-main'), 'manage_options', 'admin.php?page=acf-options-company-settings');
-  if(file_exists(THEME_LIBRARY_DIR.'/modules'))
-    add_submenu_page('admin.php?page=acf-options-theme-settings', __('Modules','theme-main'), __('Modules','theme-main'), 'manage_options', 'admin.php?page=acf-options-modules-settings');
+/**************************
+ * Initialize Modules
+ **************************/
+if(function_exists("register_field_group")){
+    register_field_group(array (
+        'id' => 'acf_modules-available',
+        'title' => 'Modules Available',
+        'fields' => $fields,
+        'location' => array (
+            array (
+                array (
+                    'param' => 'options_page',
+                    'operator' => '==',
+                    'value' => 'acf-options-modules-settings',
+                    'order_no' => 0,
+                    'group_no' => 0,
+            ),
+            ),
+        ),
+        'options' => array (
+            'position' => 'side',
+            'layout' => 'default',
+            'hide_on_screen' => array (
+            ),
+        ),
+        'menu_order' => 0,
+    ));
 }
-function div_active_pages(){
-  include(DIV_FEATURES_DIR.'/documentation.php');           #FEATURE: Documentation
-}
-
-?>
